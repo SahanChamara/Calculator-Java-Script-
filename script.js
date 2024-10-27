c3afc0e04425473ac5dd3b3c223915bdf2020374
Sahan Chamara <sahanchamara456@gmail.com>
2024-10-27 22:47:59 +0530

    sixth commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('#display');
-const buttons = document.querySelectorAll('buttons');
+const buttons = document.querySelectorAll('button');
 
 buttons.forEach((item) => {
     item.onclick = () => {
