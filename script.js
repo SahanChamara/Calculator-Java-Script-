277b383ddc278a6dba07e01cca38336ca6b0933a
Sahan Chamara <sahanchamara456@gmail.com>
2024-10-27 22:26:22 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function calc(){
-    console.log("testing");
-    
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('buttons');
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if(item.id == 'clear'){
+            display.innerText = '';
+        }else if (item.id == 'backspace'){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0,string.length-1);
+        }else if (display.innerText != '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText);
+        }else if (display.innerText == '' && item.id == 'equal'){
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        }else{
+            display.innerText += item.id;
+        }
+    }
+})
+
+const themeTogleBtn = document.querySelector('.theme-toggle');
+const calculator = document.querySelector('.calculator');
+const toggleIcon = document.querySelector('.toggle-icon');
+let isDark = true;
+themeTogleBtn.onclick = () =>{
+    calculator.classList.toggle('dark');
+    themeTogleBtn.classList.toggle('active');
+    isDark = !isDark;
 }
+
+
+
